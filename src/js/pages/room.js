ec8d45cb685ead2c469f616ae30c2a468613ed25
André Victor Macedo Pereira <victorandre280@gmail.com>
2025-02-04 12:26:13 -0300

    Alterações de resposnividade nas salas

--- src/js/pages/room.js
+++ src/js/pages/room.js
@@ -12,3 +12,20 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   });
 });
+
+function createCompetitorBoxes(count) {
+    const container = document.querySelector('.container'); // Seleciona o container onde os boxes serão adicionados
+    for (let i = 1; i <= count; i++) {
+        const competitorBox = document.createElement('div');
+        competitorBox.className = 'competitor-box text-white';
+        competitorBox.innerHTML = `
+            <img src="../assets/images/image${i}.jpeg" alt="Descrição da foto" class="competitor-image">
+            <div class="vote-count">Votos: 0</div>
+            <button class="btn btn-light btn-vote">VOTAR</button>
+            <a href="explorephoto.html" class="btn btn-light btn-explore">EXPLORAR</a>
+        `;
+        container.appendChild(competitorBox); 
+    }
+}
+
+createCompetitorBoxes(6); 
